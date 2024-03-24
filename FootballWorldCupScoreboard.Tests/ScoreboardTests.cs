97530698208975e2b7fc324fcedd9af84c0f7962
Piotr Kosmider <piotr.kosmider@cshark.com>
2024-03-24 20:51:00 +0100

    add updatescore method and tests

--- FootballWorldCupScoreboard.Tests/ScoreboardTests.cs
+++ FootballWorldCupScoreboard.Tests/ScoreboardTests.cs
@@ -30,9 +30,23 @@ public class ScoreboardTests
 
         var match = sut.GetInprogressMatches().First();
 
-
         match.AwayTeamScore.Should().Be(0);
         match.HomeTeamScore.Should().Be(0);
+    }
+
+    [Fact]
+    public void UpdateScoreTest()
+    {
+        var sut = new Scoreboard();
+
+        var matchId = sut.StartNew("Mexico", "Canada");
+
+        sut.UpdateScore(matchId, 0, 5);
+
+        var match = sut.GetInprogressMatches().First();
+
+        match.HomeTeamScore.Should().Be(0);
+        match.AwayTeamScore.Should().Be(5);
     }
 }
 
@@ -40,21 +54,39 @@ public class Scoreboard
 {
     private List<Match> _matches { get; init; } = [];
 
-    public void StartNew(string homeTeam, string awayTeam)
+    public Guid StartNew(string homeTeam, string awayTeam)
     {
         var newMath = new Match(homeTeam, awayTeam);
 
         _matches.Add(newMath);
+
+        return newMath.Id;
+    }
+
+    public void UpdateScore(Guid matchId, int homeTeamScore, int awayTeamScore)
+    {
+        var match = _matches
+            .First(x => x.Id == matchId);
+
+        match.HomeTeamScore = homeTeamScore;
+        match.AwayTeamScore = awayTeamScore;
     }
 
     public List<Match> GetInprogressMatches()
     {
-        return _matches;
+        return _matches
+            .OrderByDescending(x => x.TotalScore)
+            .ThenByDescending(x => x.StartDate)
+            .ToList();
     }
 }
 
 public class Match
 {
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public DateTime StartDate { get; init; } = DateTime.Now;
+
     public string HomeTeam { get; }
 
     public string AwayTeam { get; }
@@ -62,6 +94,8 @@ public class Match
     public int HomeTeamScore { get; set; } = 0;
 
     public int AwayTeamScore { get; set; } = 0;
+
+    public int TotalScore => HomeTeamScore + AwayTeamScore;
 
     public Match(string homeTeam, string awayTeam)
     {
