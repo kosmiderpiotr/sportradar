bf9e2597cc3b13a0c3db11a980274cc010693d02
Piotr Kosmider <piotr.kosmider@cshark.com>
2024-03-24 20:30:41 +0100

    Add Startnew method with unit tests

--- FootballWorldCupScoreboard.Tests/ScoreboardTests.cs
+++ FootballWorldCupScoreboard.Tests/ScoreboardTests.cs
@@ -1,10 +1,53 @@
+using System.Diagnostics.Metrics;
+using FluentAssertions;
+
 namespace FootballWorldCupScoreboard.Tests;
 
 public class ScoreboardTests
 {
     [Fact]
-    public void Test1()
+    public void GetInprogressMatchesTest()
     {
+        var sut = new Scoreboard();
+        sut.StartNew("Mexico", "Canada");
+        sut.StartNew("Spain", "Brazil");
+        sut.StartNew("Germany", "France");
+        sut.StartNew("Uruguay", "Italy");
+        sut.StartNew("Argentina", "Australia");
 
+        var inprogressMatchesList = sut.GetInprogressMatches();
+
+        inprogressMatchesList.Should().HaveCount(5);
+    }
+}
+
+public class Scoreboard
+{
+    private List<Match> _matches { get; init; } = [];
+
+    public void StartNew(string homeTeam, string awayTeam)
+    {
+        var newMath = new Match(homeTeam, awayTeam);
+
+        _matches.Add(newMath);
+    }
+
+    public List<Match> GetInprogressMatches()
+    {
+        return _matches;
+    }
+}
+
+public class Match
+{
+    public string HomeTeam { get; }
+
+    public string AwayTeam { get; }
+
+    public Match(string homeTeam, string awayTeam)
+    {
+        HomeTeam = homeTeam;
+
+        AwayTeam = awayTeam;
     }
 }
